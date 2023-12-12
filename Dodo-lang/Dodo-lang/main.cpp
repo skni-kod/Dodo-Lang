ecdb30d445fb8d4e8bbb27ab23b01033dac0ce7a
MichalKosiorski <118568257+MichalKosiorski@users.noreply.github.com>
2023-12-12 17:07:59 +0100

    Simple file structure
    Prepare for the gitingore test
    Description checked
    Communication checked
    Ready to commit
    3... 2... 1...

---
+++ Dodo-lang/Dodo-lang/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//Compiling process:
+	
+	/*
+	1. Read program file
+	
+	2. Lexical analysis
+
+	3. Parsing + syntax error detecting
+		- creating AST 
+
+	4. Compiling
+		- using assembly
+
+	5. Run program or create exe
+	*/
+
+	return 0;
+}
